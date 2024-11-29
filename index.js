682ec35fd7233e8a4c0a8231ee6035d2d0628717
Isabel Queiroz <belqrdev@outlook.com>
2024-11-29 20:11:58 -0300

    feat: trocando ajax para fetch api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const githubUsername = "belqr";
   const endpoint = `https://api.github.com/users/${githubUsername}`;
 
@@ -10,32 +10,34 @@ $(document).ready(function() {
   const profileFollowing = $(".numbers-item:nth-child(3)");
   const profileLink = $(".profile-link");
 
-  function getGithubProfile() {
-    $.ajax({
-      url: endpoint,
-      method: "GET",
-      beforeSend: function() {
-        profileAvatar.text("Carregando...")
-        profileName.text("Carregando...")
-        profileUsername.text("Carregando...")
-        profileRepos.find("h4 + span").text("...")
-        profileFollowers.find("h4 + span").text("...")
-        profileFollowing.find("h4 + span").text("...")
-      },
-      success: function(data) {
-        profileAvatar.attr("src", data.avatar_url);
-        profileName.text(data.name || "Nome não informado");
-        profileUsername.text(`@${data.login}`);
-        profileRepos.find("h4 + span").text(data.public_repos);
-        profileFollowers.find("h4 + span").text(data.followers);
-        profileFollowing.find("h4 + span").text(data.following);
-        profileLink.attr("href", data.html_url).text("Ver perfil");
-      },
-      error: function() {
-        alert("Erro ao carregar perfil do GitHub");
+  async function getGithubProfile() {
+    try {
+      profileAvatar.text("Carregando...");
+      profileName.text("Carregando...");
+      profileUsername.text("Carregando...");
+      profileRepos.find("h4 + span").text("...");
+      profileFollowers.find("h4 + span").text("...");
+      profileFollowing.find("h4 + span").text("...");
+
+      const response = await fetch(endpoint);
+
+      if (!response.ok) {
+        throw new Error("Erro ao carregar perfil do GitHub");
       }
-    });
+
+      const data = await response.json();
+
+      profileAvatar.attr("src", data.avatar_url);
+      profileName.text(data.name || "Nome não informado");
+      profileUsername.text(`@${data.login}`);
+      profileRepos.find("h4 + span").text(data.public_repos);
+      profileFollowers.find("h4 + span").text(data.followers);
+      profileFollowing.find("h4 + span").text(data.following);
+      profileLink.attr("href", data.html_url).text("Ver perfil");
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   getGithubProfile();
-})
+});
